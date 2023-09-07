a83cc05b727c109b3a546e7da2c705ef2b0c270f
pietra renner <pietra.renner@gmail.com>
2023-09-07 19:02:48 -0300

    commit inicial

---
+++ DiagramaVoronoi.cpp
@@ -0,0 +1,94 @@
+//
+//  DiagramaVoronoi.cpp
+//  OpenGLTest
+//
+//  Created by Márcio Sarroglia Pinho on 23/08/23.
+//  Copyright © 2023 Márcio Sarroglia Pinho. All rights reserved.
+//
+
+#include "DiagramaVoronoi.h"
+
+ifstream input;            // ofstream arq;
+
+Voronoi::Voronoi()
+{
+
+}
+Poligono Voronoi::LeUmPoligono()
+{
+    Poligono P;
+    unsigned int qtdVertices;
+    input >> qtdVertices;  // arq << qtdVertices
+    for (int i=0; i< qtdVertices; i++)
+    {
+        double x,y;
+        // Le um ponto
+        input >> x >> y;
+        Ponto(x, y).imprime();
+        if(!input)
+        {
+            cout << "Fim inesperado da linha." << endl;
+            break;
+        }
+        P.insereVertice(Ponto(x,y));
+    }
+    cout << "Poligono lido com sucesso!" << endl;
+    return P;
+}
+
+void Voronoi::LePoligonos(const char *nome)
+{
+    input.open(nome, ios::in); //arq.open(nome, ios::out);
+    if (!input)
+    {
+        cout << "Erro ao abrir " << nome << ". " << endl;
+        exit(0);
+    }
+    string S;
+
+    input >> qtdDePoligonos;
+    cout << "qtdDePoligonos:" << qtdDePoligonos << endl;
+    Ponto A, B;
+    Diagrama[0] = LeUmPoligono();
+    Diagrama[0].obtemLimites(Min, Max);// obtem o envelope do poligono
+    for (int i=1; i< qtdDePoligonos; i++)
+    {
+        Diagrama[i] = LeUmPoligono();
+        Diagrama[i].obtemLimites(A, B); // obtem o envelope do poligono
+
+        Min = ObtemMinimo (A, Min);
+        Max = ObtemMaximo (B, Max);
+
+        //printf("Diagrama[%d]: %d", i, Diagrama[i]);
+    }
+    cout << "Lista de Poligonos lida com sucesso!" << endl;
+    input.close();
+}
+
+Poligono Voronoi::getPoligono(int i)
+{
+    if (i >= qtdDePoligonos)
+    {
+        cout << "Nro de Poligono Inexistente" << endl;
+        return Diagrama[0];
+    }
+    return Diagrama[i];
+}
+unsigned int Voronoi::getNPoligonos()
+{
+    return qtdDePoligonos;
+}
+void Voronoi::obtemLimites(Ponto &min, Ponto &max)
+{
+    min = this->Min;
+    max = this->Max;
+}
+
+void Voronoi::obtemVizinhosDasArestas()
+{
+    for(int i = 0; i < getNPoligonos(); i++) {
+        for(int j = 0; j < Diagrama[i].getNVertices(); j++){
+
+        }
+    }
+}
---
+++ DiagramaVoronoi.h
@@ -0,0 +1,34 @@
+//
+//  DiagramaVoronoi.hpp
+//  OpenGLTest
+//
+//  Created by Márcio Sarroglia Pinho on 23/08/23.
+//  Copyright © 2023 Márcio Sarroglia Pinho. All rights reserved.
+//
+
+#ifndef DiagramaVoronoi_h
+#define DiagramaVoronoi_h
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#include "Poligono.h"
+
+
+class Voronoi //pode passar o que está aqui para a classe .cpp
+{
+    Poligono Diagrama[1000];
+    unsigned int qtdDePoligonos;
+    Ponto Min, Max;
+public:
+    Voronoi();
+    Poligono LeUmPoligono();
+    void LePoligonos(const char *nome);
+    Poligono getPoligono(int i); //devolve polígono de número i
+    void obtemLimites(Ponto &min, Ponto &max);
+    unsigned int getNPoligonos(); //quantos polígonos tem no diagrama
+    void obtemVizinhosDasArestas();
+};
+
+#endif /* DiagramaVoronoi_h */
